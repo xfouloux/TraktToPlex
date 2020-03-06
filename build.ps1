08217e01ea6b3ddc2df95c28777d6b3c8d213484
Sebastien Clemenceau <sebastien.clemenceau@offratel.net>
2020-03-06 17:30:46 +1100

    typo

--- build.ps1
+++ build.ps1
@@ -2,10 +2,9 @@ $ErrorActionPreference = 'Stop';
 Write-Host Starting build
 
 if ($isWindows) {
-  docker build --pull -t whoami -f Dockerfile.windows .
+	docker build --pull -t whoami -f Dockerfile.windows .
 } else {
 	docker build -t whoami --build-arg "arch=$env:ARCH" .
-  }
 }
 
 docker images
