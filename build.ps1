d88d78ed8b7ad004f4e4d6b08be453c6c2595f02
Sebastien Clemenceau <sebastien.clemenceau@offratel.net>
2020-03-06 18:14:59 +1100

    test vars

--- build.ps1
+++ build.ps1
@@ -3,8 +3,9 @@ Write-Host Starting build
 
 if ($isWindows) {
 	#build other windows versions
-	if ($env:VERSION) {
-		docker build --pull -t whoami --build-arg "version=$env:VERSION" -f Dockerfile.windows.versions . 
+	if ($env:VERSION) {.
+		Write-Host "Build windows version ${env:VERSION}"
+		docker build --pull -t whoami --build-arg "version=${env:VERSION}" -f Dockerfile.windows.versions . 
 	}else{
 		docker build --pull -t whoami -f Dockerfile.windows .
 	}
