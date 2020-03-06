c505154270eae7911afd58a027e484d462b1a1f0
Sebastien Clemenceau <sebastien.clemenceau@offratel.net>
2020-03-06 17:29:11 +1100

    only doing amd64 windows and linux build

--- build.ps1
+++ build.ps1
@@ -4,9 +4,6 @@ Write-Host Starting build
 if ($isWindows) {
   docker build --pull -t whoami -f Dockerfile.windows .
 } else {
-  if($env:ARCH == "arm"){
-	docker build -t whoami -f Dockerfile.arm --build-arg "arch=$env:ARCH" .
-  }else{
 	docker build -t whoami --build-arg "arch=$env:ARCH" .
   }
 }
