4aa8f25ba220dd36b9c7bdeb26fb95d1d2b8a757
Sebastien Clemenceau <sebastien.clemenceau@offratel.net>
2020-03-06 15:49:15 +1100

    issue with docker login

--- deploy.ps1
+++ deploy.ps1
@@ -1,4 +1,4 @@
-$ErrorActionPreference = 'Stop';
+#$ErrorActionPreference = 'Stop';
 
 if (! (Test-Path Env:\APPVEYOR_REPO_TAG_NAME)) {
   Write-Host "No version tag detected. Skip publishing."
@@ -12,7 +12,7 @@ Write-Host Starting deploy
 if (!(Test-Path ~/.docker)) { mkdir ~/.docker }
 
 '{ "experimental": "enabled" }' | Out-File ~/.docker/config.json -Encoding Ascii
-docker login -u="$env:DOCKER_USER" --password-stdin="$env:DOCKER_PASS"
+docker login -u="$env:DOCKER_USER" -p="$env:DOCKER_PASS"
 
 $os = If ($isWindows) {"windows"} Else {"linux"}
 docker tag whoami "$($image):$os-$env:ARCH-$env:APPVEYOR_REPO_TAG_NAME"
