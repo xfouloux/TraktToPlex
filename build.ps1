c3adfdc6d7a00e4ba962c148a543411e15c00fac
Sebastien Clemenceau <sebastien.clemenceau@offratel.net>
2020-03-09 17:35:58 +1100

    passed args to dockerfiles

--- build.ps1
+++ build.ps1
@@ -5,15 +5,15 @@ if ($isWindows) {
 	#build other windows versions
 	if ($env:VERSION) {
 		Write-Host "Build windows version $env:VERSION"
-		docker build -t whoami --build-arg "VERSION=$env:VERSION" --isolation=hyperv -f Dockerfile.windows.versions . 
+		docker build -t whoami --build-arg "VERSION=$env:VERSION" --build-arg "platform=$env:platform" --build-arg "dotnet_version=$env:dotnet_version" --isolation=hyperv -f Dockerfile.windows.versions . 
 	}else{
-		docker build --pull -t whoami --isolation=hyperv -f Dockerfile.windows .
+		docker build --pull -t whoami --build-arg "dotnet_version=$env:dotnet_version" --isolation=hyperv -f Dockerfile.windows .
 	}
 } else {
 	if ($env:ARCHVERSION) {
-		docker build -t whoami -f "Dockerfile.$env:ARCH" --build-arg "archversion=$env:ARCHVERSION" .
+		docker build -t whoami -f "Dockerfile.$env:ARCH" --build-arg "archversion=$env:ARCHVERSION" --build-arg "platform=$env:platform" --build-arg "dotnet_version=$env:dotnet_version" .
 	}else{
-		docker build -t whoami --build-arg "arch=$env:ARCH" .
+		docker build -t whoami --build-arg "arch=$env:ARCH" --build-arg "platform=$env:platform" --build-arg "dotnet_version=$env:dotnet_version" .
 	}
 }
 
