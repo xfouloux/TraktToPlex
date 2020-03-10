d268baab63840549962c158ac913ce2e58b23699
Sebastien Clemenceau <sebastien.clemenceau@offratel.net>
2020-03-10 12:19:11 +1100

    try to fix arm

--- build.ps1
+++ build.ps1
@@ -11,12 +11,9 @@ if ($isWindows) {
 	}
 } else {
 	if ($env:ARCHVERSION) {
-		sudo apt-get install qemu binfmt-support qemu-user-static
+		sudo apt-get -y install qemu binfmt-support qemu-user-static
 		# This step will execute the registering scripts
-		docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-		# Let's test the emulation again
-		docker run --rm -t arm64v8/ubuntu uname -m 
-		
+		docker run --rm --privileged multiarch/qemu-user-static --reset -p yes	
 		docker build -t whoami -f "Dockerfile.$env:ARCH" --build-arg "archversion=$env:ARCHVERSION" --build-arg "platform=$env:platform" --build-arg "dotnet_version=$env:dotnet_version" .
 	}else{
 		docker build -t whoami --build-arg "platform=$env:platform" --build-arg "dotnet_version=$env:dotnet_version" .
